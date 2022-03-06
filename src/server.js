a42dfaeb931fa527802307bb599a46245e4f2847
yh9191 <78723464+yh9191@users.noreply.github.com>
2022-03-06 22:56:32 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+const PORT = 4000;
+const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
+    next();
+};
+
+const handleHome = (req, res) => {
+    return res.send("<h1>Home Response!</h1>");
+}
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () => console.log(`Server listening on Port http://localhost:${PORT} 😃`);
+app.listen(PORT, handleListening);
