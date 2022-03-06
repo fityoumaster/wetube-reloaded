674000cbfc6003092dc8b8e54fed1616833ba393
yh9191 <78723464+yh9191@users.noreply.github.com>
2022-03-07 01:23:15 +0900

    morgan 미들웨어 패키지 설치/적용

--- src/server.js
+++ src/server.js
@@ -1,31 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+
 const PORT = 4000;
+
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue.");
-    next();
-};
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
     return res.send("<h1>Home Response!</h1>");
 }
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.");
+const login = (req, res) => {
+    return res.send("login");
 }
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => console.log(`Server listening on Port http://localhost:${PORT} 😃`);
 app.listen(PORT, handleListening);
