2c644ad27685833bb862afb62ce6f903d1a83465
fityoumaster <fityoumaster@gamil.com>
2022-03-08 12:47:31 +0900

    pug extends / block / controller var > view

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-    return res.send(req.params);
-}
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
