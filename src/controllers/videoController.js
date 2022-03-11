b798dcf65b5efd3370cada183eb134da0a30db3a
fityoumaster <fityoumaster@gamil.com>
2022-03-11 17:32:36 +0900

    /lectures/2669

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,30 +4,53 @@ export const home = async (req, res) => {
     const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
 }
+
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });    
+    }
     return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing` });
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });    
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({_id:id});
+    if(!video){
+        return res.render("404", { pageTitle: "Video not found." });    
+    }
+    await Video.findByIdAndUpdate(id,{
+        title,
+        description,
+        hashtags: hashtags
+            .split(",")
+            .map((word) => word.startsWith("#") ? word : `#${word}`)
+    });
     return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
     const { title, description, hashtags } = req.body;
     try{
         const video = new Video({
             title: title,
             description: description,
-            hashtags: hashtags.split(",").map((word) => `#${word}`)
+            hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`)
         });
         await video.save();
         return res.redirect("/");
@@ -37,5 +60,4 @@ export const postUpload = async (req, res) => {
             errorMessage: error._message
          });
     }
-    
 };
