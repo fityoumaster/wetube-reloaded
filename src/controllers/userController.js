41ed43c3466a13e06de31a1d09cf1324249ba5ea
fityoumaster <fityoumaster@gamil.com>
2022-03-14 18:06:55 +0900

    user 컨트롤러 계정/비밀번호 유효성 체크

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,22 @@ import User from "../models/User.js";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-    const {name, username, email, password, location} = req.body;
+    const {name, username, email, password, password2, location} = req.body;
+    const pageTitle = "join";
+    if(password !== password2){
+        return res.render("join", { 
+            pageTitle,
+            errorMessage: "Password confirmation dose not match.",
+        });
+    }
+    /* $or : 둘 중 하나라도 있다면 true를 반환합니다. */
+    const exists = await User.exists({ $or: [{username}, {email}] });
+    if(exists){
+        return res.render("join", { 
+            pageTitle,
+            errorMessage: "This username/email is already taken.",
+        });
+    }
     await User.create({
         name, 
         username, 
